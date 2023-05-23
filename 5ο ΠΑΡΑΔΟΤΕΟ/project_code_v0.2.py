1e4892cc3ac6b404c888c8f4f6dc551d995896be
TrueThanos <staletosigma@gmail.com>
2023-05-23 17:50:36 +0300

    add speakerOverlay class
    plus hide widgets after submit

--- 5ο ΠΑΡΑΔΟΤΕΟ/project_code_v0.2.py
+++ 5ο ΠΑΡΑΔΟΤΕΟ/project_code_v0.2.py
@@ -12,20 +12,20 @@ class RoomDialog:
 
     def create_widgets(self):
         # Label and Entry for room width
-        width_label = tk.Label(self.window, text="Width:")
-        width_label.pack()
+        self.width_label = tk.Label(self.window, text="Width:")
+        self.width_label.pack()
         self.width_entry = tk.Entry(self.window)
         self.width_entry.pack()
 
         # Label and Entry for room length
-        length_label = tk.Label(self.window, text="Length:")
-        length_label.pack()
+        self.length_label = tk.Label(self.window, text="Length:")
+        self.length_label.pack()
         self.length_entry = tk.Entry(self.window)
         self.length_entry.pack()
 
         # Submit button
-        submit_button = tk.Button(self.window, text="Submit", command=self.submit_form)
-        submit_button.pack()
+        self.submit_button = tk.Button(self.window, text="Submit", command=self.submit_form)
+        self.submit_button.pack()
 
         self.room_space = RoomSpace(self.window)
 
@@ -33,6 +33,16 @@ class RoomDialog:
         # Get room dimensions from user input
         self.room_width = int(self.width_entry.get())
         self.room_length = int(self.length_entry.get())
+
+        self.length_entry.destroy()
+        self.length_label.destroy()
+        self.width_entry.destroy()
+        self.width_label.destroy()
+        self.submit_button.destroy()
+
+        l = tk.Label(self.window, text = "Click left to reserve space, click right to add speakers")
+        l.config(font =("Courier", 14))
+        l.pack(side='left')
 
         # Draw the grid
         self.room_space.initialize(self.room_width, self.room_length)
@@ -48,14 +58,18 @@ class RoomSpace:
         self.length = None
 
         self.reserved_positions = []
+        self.speaker_overlay = SpeakerOverlay()
 
         # Canvas for grid visualization
         self.canvas = tk.Canvas(self.window, width=1200, height=400)
         self.canvas.pack(expand=True, fill=tk.BOTH)
 
     def initialize(self, room_width, room_length):
-        # Bind mouse click event
+        # Bind mouse click event - left click
         self.canvas.bind("<Button-1>", self.on_canvas_click)
+        # Bind mouse click event - right click
+        self.canvas.bind("<Button-3>", self.on_canvas_click)
+
         self.width = room_width
         self.length = room_length
 
@@ -78,6 +92,8 @@ class RoomSpace:
 
                 if (i, j) in self.reserved_positions:
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill="blue", outline="black")
+                elif (i, j) in self.speaker_overlay.speaker_positions:
+                    self.canvas.create_rectangle(x1, y1, x2, y2, fill="red", outline="black")
                 else:
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill="white", outline="black")
 
@@ -86,17 +102,32 @@ class RoomSpace:
         canvas_width = self.canvas.winfo_width()
         canvas_height = self.canvas.winfo_height()
         grid_size = min(canvas_width / self.width, canvas_height / self.length)
+
         grid_x = int(event.x // grid_size)
         grid_y = int(event.y // grid_size)
 
         # Toggle the color of the clicked grid square
-        if (grid_y, grid_x) not in self.reserved_positions:
+        if event.num == 1 and (grid_y, grid_x) not in self.reserved_positions:
             self.reserved_positions.append(tuple((grid_y, grid_x)))
-        else:
+        elif event.num == 1:
             self.reserved_positions.remove((grid_y, grid_x))
+        elif event.num == 3 and (grid_y, grid_x) not in self.speaker_overlay.speaker_positions:
+            self.speaker_overlay.add_speaker(grid_y, grid_x)
+        elif event.num == 3:
+            self.speaker_overlay.remove_speaker(grid_y, grid_x)
 
         # Redraw the grid
         self.draw_room()
+
+class SpeakerOverlay:
+    def __init__(self):
+        self.speaker_positions = []
+
+    def add_speaker(self, y, x):
+        self.speaker_positions.append(tuple((y, x)))
+
+    def remove_speaker(self, y, x):
+        self.speaker_positions.remove((y, x))
 
 # Create and run the application
 app = RoomDialog()
